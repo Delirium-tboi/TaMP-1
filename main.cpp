1f3d92264c431a9ca221475c98c617bbf2231dcc
Delirium-tboi <semin.sasha0920@yandex.ru>
2025-09-14 20:08:41 +0300

    Добавление main.cpp, modAlphaCipher.cpp, modAlphaCipher.h

---
+++ main.cpp
@@ -0,0 +1,88 @@
+#include "modAlphaCipher.h"
+#include <iostream>
+#include <algorithm>
+#include <locale>
+#include <codecvt>
+#include <cwctype>
+
+using namespace std;
+
+// проверка, чтобы строка состояла только из русских букв
+bool isValid(const wstring& s)
+{
+    for(auto c : s) {
+        if(!iswalpha(c) || !iswupper(c)) {
+            return false;
+        }
+        // Проверяем, что символ находится в русском алфавите
+        if(c < L'А' || c > L'Я') {
+            if(c != L'Ё') // отдельно проверяем Ё
+                return false;
+        }
+    }
+    return true;
+}
+
+// Функция для преобразования string в wstring
+wstring to_wide(const string& narrow_str) {
+    wstring_convert<codecvt_utf8<wchar_t>> converter;
+    return converter.from_bytes(narrow_str);
+}
+
+// Функция для преобразования wstring в string
+string to_narrow(const wstring& wide_str) {
+    wstring_convert<codecvt_utf8<wchar_t>> converter;
+    return converter.to_bytes(wide_str);
+}
+
+int main()
+{
+    setlocale(LC_ALL, "ru_RU.UTF-8");
+    
+    string key_input;
+    string text_input;
+    unsigned op;
+    
+    cout << "Шифр готов. Введите ключ: ";
+    cin >> key_input;
+    
+    wstring key = to_wide(key_input);
+    transform(key.begin(), key.end(), key.begin(), ::towupper);
+    
+    if(!isValid(key)) {
+        cerr << "Ключ недействителен! Используйте только русские буквы в верхнем регистре.\n";
+        return 1;
+    }
+    
+    cout << "Ключ загружен\n";
+    modAlphaCipher cipher(key);
+    
+    do {
+        cout << "Шифр готов. Выберите операцию (0-выход, 1-шифрование, 2-расшифрование): ";
+        cin >> op;
+        
+        if(op > 2) {
+            cout << "Неверная операция\n";
+        } else if(op > 0) {
+            cout << "Введите текст: ";
+            cin >> text_input;
+            
+            wstring text = to_wide(text_input);
+            transform(text.begin(), text.end(), text.begin(), ::towupper);
+            
+            if(isValid(text)) {
+                if(op == 1) {
+                    wstring encrypted = cipher.encrypt(text);
+                    cout << "Зашифрованный текст: " << to_narrow(encrypted) << endl;
+                } else {
+                    wstring decrypted = cipher.decrypt(text);
+                    cout << "Расшифрованный текст: " << to_narrow(decrypted) << endl;
+                }
+            } else {
+                cout << "Операция отменена: неверный текст. Используйте только русские буквы.\n";
+            }
+        }
+    } while(op != 0);
+    
+    return 0;
+}
